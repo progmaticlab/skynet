c53185444adb8484ee4d0b45576ff8690b4d1ee0
Alexandre Levine <alevine@alevine-mbp.jnpr.net>
2020-02-19 20:51:01 +0300

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# $1 - Time in seconds or number of curls if sleep is 0
+# $2 - Sleep time
+function request() {
+        if [ $2 -ne 0 ]
+        then 
+                n=$(echo $1 / $2 | bc)
+        else
+                n=$1
+        fi
+        for ((i=0; i<n; i++))
+        do
+                curl "http://10.110.158.9:9080/productpage"
+                echo "Curling: ", $1, $2, $i
+                sleep $2
+        done
+}
+
+while true
+do
+        request 3 0.2
+        request 30 0
+#        request 30 0.2
+#        request 30 0.01
+#        request 30 0.1
+#        request 30 0.01
+done
+q
