fc000a9c7b06b815b3172450dcbe31899f3df07b
Ubuntu <ubuntu@node1.cluster.local>
2020-03-03 17:35:20 +0000

    Unified timestamp for all pods in one batch

--- envoy_stats.sh
+++ envoy_stats.sh
@@ -2,13 +2,12 @@
 
 while true
 do
-	d=`date -Iseconds`
+	d=$(date -Iseconds)
 	kubectl describe pods | grep -w "Name:\|Node:" > "pods.$d"
 	for pod in `kubectl get pods --no-headers -o custom-columns=":metadata.name" --field-selector=status.phase=Running` 
 	do 
 		#kubectl exec -it $pod  -c istio-proxy  -- sh -c 'curl localhost:15000/stats' | gzip > $pod.$d.gz
-		echo "Storing data for pod $pod"
+		echo "Storing data for pod $pod.$d"
 		kubectl exec $pod  -c istio-proxy  -- sh -c 'curl localhost:15000/stats | grep 9080' > $pod.$d
 	done
-	sleep 5
 done
